f72cfd8697b8f73533f3a8c20b4e0240aa13da83
Wonjune Kang <wjkang@mit.edu>
2025-05-14 15:28:45 -0400

    Updated inference to enable Llama 3.2 option in addition to MiniChat

--- inference.py
+++ inference.py
@@ -2,11 +2,17 @@ import argparse
 import librosa
 import torch
 from omegaconf import OmegaConf
-from transformers import LlamaTokenizer, AutoTokenizer, HubertForCTC
+from transformers import AutoTokenizer
 
 from model.audio_encoder import AudioEncoder
 from model.audio_llama import AudioLlamaForCausalLM
-from utils import merge_prompt_tokens, PROMPT_PREFIX, PROMPT_SUFFIX
+from utils import (
+    merge_prompt_tokens,
+    LLAMA_PROMPT_PREFIX,
+    LLAMA_PROMPT_SUFFIX,
+    MINICHAT_PROMPT_PREFIX,
+    MINICHAT_PROMPT_SUFFIX,
+)
 
 
 class LLMSpeechTextInference():
@@ -16,80 +22,35 @@ class LLMSpeechTextInference():
 
         # Audio encoder.
         checkpoint = torch.load(audio_encoder_checkpoint, map_location="cpu")
-        self.audio_encoder = AudioEncoder(self.config)
+        self.audio_encoder = AudioEncoder(self.config, self.device)
         self.audio_encoder.load_state_dict(checkpoint)
         self.audio_encoder.eval().to(self.device)
         print("Loaded audio encoder.\n")
 
         # LLM tokenizer.
-        self.llm_tokenizer = LlamaTokenizer.from_pretrained(
-            "GeneZC/MiniChat-2-3B",
+        self.llm_type = self.config.model.llm_type
+        self.llm_tokenizer = AutoTokenizer.from_pretrained(
+            self.llm_type,
             use_fast=False,
+            padding_side="left",
         )
+        self.llm_tokenizer.pad_token = self.llm_tokenizer.eos_token
+
+        if "llama" in self.llm_type:
+            self.prompt_prefix = LLAMA_PROMPT_PREFIX
+            self.prompt_suffix = LLAMA_PROMPT_SUFFIX
+        else:
+            self.prompt_prefix = MINICHAT_PROMPT_PREFIX
+            self.prompt_suffix = MINICHAT_PROMPT_SUFFIX
 
         # Load and freeze LLM model weights.
         self.llm = AudioLlamaForCausalLM.from_pretrained(
-            "GeneZC/MiniChat-2-3B",
+            self.llm_type,
             use_cache=True,
             torch_dtype=torch.float16,
         ).eval()
         self.llm.to(self.device)
         print("Loaded LLM.\n")
-
-        # Load HuBERT ASR model for getting CTC offsets.
-        self.hubert_tokenizer = AutoTokenizer.from_pretrained("facebook/hubert-large-ls960-ft")
-        self.hubert = HubertForCTC.from_pretrained("facebook/hubert-large-ls960-ft").to(device)
-        self.hubert.to(self.device)
-        print("Loaded HuBERT.\n")
-
-    def perform_hubert_asr(self, audio):
-        # Feed audio through model to get greedily predicted transcription IDs.
-        logits = self.hubert(audio).logits[0]
-        pred_ids = torch.argmax(logits, axis=-1)
-
-        # Decode transcription IDs to get text transcript.
-        # NOTE: Always converts to lower case.
-        transcript = self.hubert_tokenizer.decode(pred_ids).lower()
-        return transcript
-
-    def get_ctc_pool_ranges(self, audio, pool_range=4):
-        # Feed audio through model to get greedily predicted transcription IDs.
-        logits = self.hubert(audio).logits[0]
-        pred_ids = torch.argmax(logits, axis=-1)
-
-        # Perform decoding to get CTC offsets for each predicted word.
-        outputs = self.hubert_tokenizer.decode(pred_ids, output_word_offsets=True)
-        word_offsets = outputs.word_offsets
-        ctc_word_offsets = [
-            (word['start_offset'], word['end_offset']) for word in word_offsets
-        ]
-
-        # Add offset ranges for silence in between words. The first element of
-        # each tuple is a flag denoting whether the offset corresponds to
-        # a word (1) or silence (0).
-        all_word_offsets = [(0, 0, ctc_word_offsets[0][0])]
-        for i in range(len(ctc_word_offsets)-1):
-            all_word_offsets.append((1, ctc_word_offsets[i][0], ctc_word_offsets[i][1]))
-            all_word_offsets.append((0, ctc_word_offsets[i][1], ctc_word_offsets[i+1][0]))
-        all_word_offsets.append((1, ctc_word_offsets[-1][0], ctc_word_offsets[-1][1]))
-        all_word_offsets.append(
-            (0, ctc_word_offsets[-1][1], ctc_word_offsets[-1][1] + (pool_range * 2))
-        )
-
-        # Aggregate the offsets into pooling ranges for the audio encoder.
-        ctc_pool_ranges = []
-        for is_word, start_offset, end_offset in all_word_offsets:
-            if is_word == 1:
-                startpoint = start_offset
-                endpoint = start_offset + pool_range
-                while startpoint < end_offset:
-                    ctc_pool_ranges.append((startpoint, endpoint))
-                    startpoint += pool_range
-                    endpoint += pool_range
-            else:
-                ctc_pool_ranges.append((start_offset, end_offset))
-
-        return ctc_pool_ranges
 
     def generate_llm_response(self, inputs_embeds, max_new_tokens=256):
         with torch.no_grad():
@@ -114,7 +75,7 @@ class LLMSpeechTextInference():
 
     def generate_text_response(self, input_text, max_new_tokens=256):
         # Create full prompt for instruction-tuned LLM.
-        full_text_prompt = f"{PROMPT_PREFIX} {input_text}{PROMPT_SUFFIX} "
+        full_text_prompt = f"{self.prompt_prefix} {input_text}{self.prompt_suffix} "
 
         with torch.no_grad():
             # Tokenize and get embeddings for the full text prompt.
@@ -131,32 +92,19 @@ class LLMSpeechTextInference():
 
         return llm_response
 
-    def generate_asr_cascade_response(self, audio, additional_text_prompt="", max_new_tokens=256):
-        with torch.no_grad():
-            # Perform ASR using HuBERT.
-            audio_tensor = torch.tensor(audio).float().unsqueeze(0).to(self.device)
-            asr_transcript = self.perform_hubert_asr(audio_tensor)
-
-            # Combine the transcript with any additional text prompt.
-            # NOTE: Assumes that the text prompt always comes before the
-            # transcribed text.
-            full_text = additional_text_prompt + asr_transcript
-            llm_response = self.generate_text_response(full_text, max_new_tokens)
-
-        return llm_response
-
     def generate_audio_response(self, audio, additional_text_prompt="", max_new_tokens=256):
         with torch.no_grad():
-            audio_tensor = torch.tensor(audio).float().unsqueeze(0).to(self.device)
+            audio_tensor = torch.tensor(audio).half().unsqueeze(0).to(self.device)
 
-            if self.audio_encoder.downsample_method == "ctc_pool":
-                # Get the CTC pooling ranges for the audio.
-                ctc_pool_ranges = self.get_ctc_pool_ranges(audio_tensor)
+            with torch.autocast(device_type='cuda', dtype=torch.float16):
+                if self.audio_encoder.downsample_method == "ctc_pool":
+                    # Get the CTC pooling ranges for the audio.
+                    ctc_pool_ranges = self.get_ctc_pool_ranges(audio_tensor)
 
-                # Get embeddings from the audio encoder.
-                audio_embeds = self.audio_encoder(audio_tensor, [ctc_pool_ranges])
-            else:
-                audio_embeds = self.audio_encoder(audio_tensor, ctc_pool_ranges=None)
+                    # Get embeddings from the audio encoder.
+                    audio_embeds = self.audio_encoder(audio_tensor, [ctc_pool_ranges])
+                else:
+                    audio_embeds = self.audio_encoder(audio_tensor, ctc_pool_ranges=None)
 
             # Combine the audio embeddings with any additional text prompt.
             # NOTE: Currently assumes that the text prompt always comes before
@@ -181,6 +129,7 @@ class LLMSpeechTextInference():
                 inputs_embeds=combined_embeds,
                 tokenizer=self.llm_tokenizer,
                 embed_tokens=self.llm.model.embed_tokens,
+                llm_type=self.llm_type,
                 device=self.device,
             )
             llm_response = self.generate_llm_response(prompt_emb_sequence, max_new_tokens)[0]
@@ -227,4 +176,3 @@ if __name__ == '__main__':
 
     print("LLM Response:\n")
     print(llm_response)
-
